5272eccf3b63eb59744c0856c49d23eb35e7c3de
Ahmad Wada' Syaifudin <ahmad.wada@dwebsite.net>
2021-01-11 14:29:26 +0700

    add conference

--- Security/Authentication/Provider/OAuthProvider.php
+++ Security/Authentication/Provider/OAuthProvider.php
@@ -18,20 +18,28 @@ use OAuth2\OAuth2;
 use OAuth2\OAuth2AuthenticateException;
 use OAuth2\OAuth2ServerException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Authentication\Provider\AuthenticationProviderInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AccountStatusException;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-
+use Symfony\Component\HttpFoundation\Request;
+use App\Infrastructure\Symfony\Utils\Connection\ConferenceConnectionHandler;
+use App\Infrastructure\Symfony\Utils\Connection\ConferenceDatabaseConnection;
+use App\Infrastructure\Symfony\Utils\SiteManager;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
+use Doctrine\Persistence\ManagerRegistry;
 /**
  * OAuthProvider class.
  *
  * @author  Arnaud Le Blanc <arnaud.lb@gmail.com>
  */
-class OAuthProvider implements AuthenticationProviderInterface
+class OAuthProvider  implements AuthenticationProviderInterface
 {
+    #const CENTRALS = array('as1','as2','as3'); 
     /**
      * @var UserProviderInterface
      */
@@ -55,6 +63,7 @@ class OAuthProvider implements AuthenticationProviderInterface
         $this->userProvider = $userProvider;
         $this->serverService = $serverService;
         $this->userChecker = $userChecker;
+        
     }
 
     /**
@@ -64,6 +73,7 @@ class OAuthProvider implements AuthenticationProviderInterface
      */
     public function authenticate(TokenInterface $token)
     {
+
         if (!$this->supports($token)) {
             // note: since strict types in PHP 7, return; and return null; are not the same
             // Symfony's interface says to "never return null", but return; is still technically null
@@ -77,20 +87,23 @@ class OAuthProvider implements AuthenticationProviderInterface
             // TODO: this is nasty, create a proper interface here
             /** @var OAuthToken&TokenInterface&\OAuth2\Model\IOAuth2AccessToken $accessToken */
             $accessToken = $this->serverService->verifyAccessToken($tokenString);
-
             $scope = $accessToken->getScope();
             $user = $accessToken->getUser();
-
+            $conference = $accessToken->getConferenceId();
+            
             if (null !== $user) {
+                
+                $this->userChecker->setConference($conference);
                 try {
                     $this->userChecker->checkPreAuth($user);
                 } catch (AccountStatusException $e) {
+                    //dd($e);
                     throw new OAuth2AuthenticateException(Response::HTTP_UNAUTHORIZED, OAuth2::TOKEN_TYPE_BEARER, $this->serverService->getVariable(OAuth2::CONFIG_WWW_REALM), 'access_denied', $e->getMessage());
                 }
 
                 $token->setUser($user);
             }
-
+            
             $roles = (null !== $user) ? $user->getRoles() : [];
 
             if (!empty($scope)) {
@@ -100,7 +113,7 @@ class OAuthProvider implements AuthenticationProviderInterface
             }
 
             $roles = array_unique($roles, SORT_REGULAR);
-
+            
             $token = new OAuthToken($roles);
             $token->setAuthenticated(true);
             $token->setToken($tokenString);
